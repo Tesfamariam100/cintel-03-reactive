936cceeb0bb32c6cf8f6b783edaa0ebf2047fba1
Tesfamariam <156039404+Tesfamariam100@users.noreply.github.com>
2024-03-22 03:23:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 from shiny.express import input, ui
 from shinywidgets import render_plotly
-from shiny import reactive, render, req
+from shiny import reactive, render
 import seaborn as sns
 import pandas as pd
 import palmerpenguins
@@ -12,14 +12,14 @@ penguins_df = palmerpenguins.load_penguins()
 # Add a reactive calculation to filter the data
 @reactive.calc
 def filtered_data():
-    return penguins_df
+    return penguins_df[penguins_df["species"].isin(input.selected_species_list())]
 
 # Set the page title
-ui.page_opts(title="Penguins Data Tesfamariam")
+ui.page_opts(title="Penguins Data Tesfamariam", fillable=True)
 
 # Create a Shiny UI sidebar
 with ui.sidebar(open="open"):
-    ui.h2("Sidebar")
+    ui.h4("Sidebar")
     ui.input_selectize("selected_attribute", 
                        "Select Attribute",
                        ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"]
@@ -30,19 +30,19 @@ with ui.sidebar(open="open"):
                              ["Adelie", "Gentoo", "Chinstrap"],
                              selected=["Adelie"], inline=False)
     ui.hr()
-    ui.a("My GitHub", href="https://github.com/Tesfamariam100/cintel-02-data", target="_blank")
+    ui.a("My GitHub Repo", href="https://github.com/Tesfamariam100/cintel-03-reactive", target="_blank")
 
 # Display a DataTable and a Data Grid
 with ui.layout_columns():
     with ui.card(full_screen=True):
-        ui.h2("Penguin Data Table")
+        ui.h4("Penguin Data Table")
         with ui.div(style="height: 400px; overflow-y: auto;"):
             @render.data_frame
             def penguins_datatable():
                 return render.DataTable(filtered_data()) 
 
     with ui.card(full_screen=True):
-        ui.h2("Penguin Data Grid")
+        ui.h4("Penguin Data Grid")
         with ui.div(style="height: 400px; overflow-y: auto;"):
             @render.data_frame
             def penguins_grid():
@@ -54,25 +54,26 @@ ui.hr()
 # Create a layout for the graphs
 with ui.layout_columns():
     with ui.card(full_screen=True):
-        ui.card_header("Plotly Graphs: Species")
-        with ui.navset_card_tab(id="tab"):
-            with ui.nav_panel("Plotly Histogram"):
-                @render_plotly
-                def plotly_histogram():
-                    return px.histogram(filtered_data(), 
-                                        x=input.selected_attribute(),
-                                        nbins=input.plotly_bin_count(),
-                                        color="species")
+        ui.card_header("Plotly Histogram")
+        @render_plotly
+        def plotly_histogram():
+            return px.histogram(filtered_data(), 
+                                x=input.selected_attribute(),
+                                nbins=input.plotly_bin_count(),
+                                color="species")
 
-            with ui.nav_panel("Seaborn Histogram"):
-                @render.plot
-                def seaborn_histogram():
-                    return sns.histplot(filtered_data(), 
-                                        x="species", kde=False)
+    with ui.card(full_screen=True):
+        ui.card_header("Seaborn Histogram")
+        @render.plot
+        def seaborn_histogram():
+            return sns.histplot(filtered_data(),
+                          x="species", kde=True), 
+        
 
-            with ui.nav_panel("Plotly Scatterplot"):
-                @render_plotly
-                def plotly_scatterplot():
-                    return px.scatter(filtered_data(),
-                                      x="flipper_length_mm", y="bill_length_mm", color="species", 
-                                      title="Plotly Scatterplot: Species")
+    with ui.card(full_screen=True):
+        ui.card_header("Plotly Scatterplot")
+        @render_plotly
+        def plotly_scatterplot():
+            return px.scatter(filtered_data(),
+                              x="flipper_length_mm", y="bill_length_mm", color="species", 
+                              title="Plotly Scatterplot: Species")
